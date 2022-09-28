4d951849b9628442fffbe4c05297c91e080b02e1
Justin Ho <hochungyan@live.hk>
2022-09-28 16:02:50 +0100

    Add Basket

---
+++ test/testing-framework.js
@@ -0,0 +1,20 @@
+// Matcher functions
+const assertEquals = (actualResult, expectedResult) => actualResult === expectedResult;
+const assertTrue = actualResult => actualResult === true;
+const assertFalse = actualResult => actualResult === false;
+function assertNull(actualResult) {
+    return actualResult === null;
+}
+
+module.exports = {
+    assertEquals,
+    assertTrue,
+    assertFalse,
+    assertNull
+};
+
+// ==   same value
+// === same value and same type
+// usually favour ===
+// 2 == 2;   // true
+// 2 === 2;  // false
