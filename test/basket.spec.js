9e5fee7be6ca0863489e4fffc7e3f165b58ff47c
Justin Ho <hochungyan@live.hk>
2022-09-29 12:18:57 +0100

    Initial try for Test 3

--- test/basket.spec.js
+++ test/basket.spec.js
@@ -5,7 +5,7 @@ let expected;
 let actual;
 let result;
 let basket;
-let item;
+let item1;
 
 // Test 1 - is anything added to the basket?
 
@@ -14,11 +14,11 @@ console.log(`Test 1 - add puts something into the basket`);
 
 // Arrange
 basket = new Basket(); //Setup New Basket
-item = { id: `item1` }; //Setup Inital item
+item1 = { id: `item1` }; //Setup Inital item
 expected = 1; //Expected value
 
 // Act
-basket.add(item); //Add 1 item
+basket.add(item1); //Add 1 item
 actual = basket.basketItems.length; //Items in the basket
 
 // Assert
@@ -32,32 +32,51 @@ expected = undefined;
 actual = undefined;
 result = undefined;
 
+// Test 2 - is something removed from the basket?
 console.log(`============================`);
 console.log(`Test 2: Remove Item from the basket`)
+
 // Arrange
 basket = new Basket();
-let item1 = { id: `item1` };
 let item2 ={id:`item2`};
 basket.add(item1);
 basket.add(item2);
 expected = 1
 actual = basket.basketItems.length;
 console.log(`Test 2: Number of Item in the basket Before: ${actual}`);
+
 //Act
-basket.remove(item);
+basket.remove(item1);
 actual = basket.basketItems.length;
 console.log(`Test 2: Number of Item in the basket After: ${actual}`);
+
 //Assert
 result = assertEquals(actual, expected);
 console.log(`Test 2: Does the basket contains ${expected} item: ${result}`);
-console.log(`============================`);
 
 // Clean up
 basket = null;
-item = null;
+item = undefined;
+item1 = undefined;
+item2 = undefined;
 expected = undefined;
 actual = undefined;
 result = undefined;
 
+//Test 3 Check when the basket is full or not
+console.log(`============================`);
+console.log(`Test 3: Check Bag Capacity`)
 
+//Arrange
+basket = new Basket(3);
+item1 = { id: `item1` };
+item2 ={id:`item2`};
+let item3 = {id:`item3`};
+expected = 3;
+basket.add(item1);
+basket.add(item2);
+basket.add(item3);
+actual =basket.basketItems.length;
+result = assertEquals(actual,expected);
+console.log(`Is the Bag Full: ${result}`);
 
