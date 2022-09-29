6279dc1cdc43efc9ae30d33d0192fafe1b512759
Justin Ho <hochungyan@live.hk>
2022-09-29 16:40:27 +0100

    Test 3 Completed

--- src/basket.js
+++ src/basket.js
@@ -2,15 +2,25 @@ class Basket {
 
     constructor(capacity) {
         this.capacity = capacity;
-        this.items = [];
+        this.basketItems = [];
     }
 
     add = item => {
+        if (this.isFull())
+        {
+            return "Sorry, this basket is full";
+        }
+        else {
         this.basketItems.push(item);
+        return this.basketitems;
+        }
     };
+
+
     remove = item => {
         this.basketItems.pop(item);
     };
+    
     isFull() {
         return this.basketItems.length >= this.capacity;
       }
