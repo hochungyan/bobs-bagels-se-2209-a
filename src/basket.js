744b49c7427d537d9a6e3b70aa2bd307fe412a81
Justin Ho <hochungyan@live.hk>
2022-09-30 01:58:51 +0100

    Test5A completed with Explanation for my Noob Miss UK

--- src/basket.js
+++ src/basket.js
@@ -20,12 +20,15 @@ class Basket {
 
 
     remove = item => {
+        //When I try to remove an item from my basket, if it existed in the basket then the method will run and remove that item.
+        // [].includes(value); 
+        //Include will return true, if the given value found at least once in the array... In this case, value would be either item1 or item2 
         if(this.basketItems.includes(item))
         {
         this.basketItems.pop(item);
         }
         else
-        {
+        {   //Otherwise it will return 'Sorry, item not in basket'
             return 'Sorry, item not in basket';
         }
     };
